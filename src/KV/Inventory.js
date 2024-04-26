bd457f6b835cc0d099ab93182cf9ed63c7d011b8
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-04-26 16:46:25 +0800

    Add files via upload

---
+++ src/KV/Inventory.js
@@ -0,0 +1,15 @@
+import{Key1,key2,key3,Key4,Key5,Key6,Key7,Key8,key9}
+    from '../KV/Inventory'
+export {Key1,key2,key3,Key4,Key5,Key6,Key7,Key8,key9}
+
+function HandleKey(){
+this.Key1 = new HandleKey()
+this.Key2 = new HandleKey()
+this.Key3 = new HandleKey() 
+this.Key4 = new HandleKey()
+this.Key5 = new HandleKey()
+this.Key6 = new HandleKey() 
+this.Key7 = new HandleKey()
+this.Key8 = new HandleKey()
+this.Key9 = new HandleKey() 
+}
