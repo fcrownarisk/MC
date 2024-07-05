d9feedfcc78e096f0b1897b8aca0843c7956c6b7
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-05 07:48:14 -0700

    Update useStore.js

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,10 +1,7 @@
 import create from 'zustand'
 import { nanoid } from 'nanoid'
-
 const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key))
 const setLocalStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
-
-
 export const useStore = create((set) => ({
 	texture: 'dirt',
 	cubes: getLocalStorage('cubes') || [],
